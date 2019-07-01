7369a2fdcc763bd06648b5cb1c0b9e5b61c0989c
Jeon Sumin <bubble_030@khu.ac.kr>
2019-07-02 02:35:57 +0900

    add ContentsRecyclerViewAdapter, ContentsOverviewData, rv_item_contents

--- app/src/main/java/com/computer/inu/readit_appjam/Fragment/HomeFragment.kt
+++ app/src/main/java/com/computer/inu/readit_appjam/Fragment/HomeFragment.kt
@@ -3,11 +3,17 @@ package com.computer.inu.readit_appjam.Fragment
 
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.support.v7.widget.GridLayoutManager
+import android.support.v7.widget.LinearLayoutManager
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.RelativeLayout
+import com.computer.inu.readit_appjam.Adapter.ContentsRecyclerViewAdapter
+import com.computer.inu.readit_appjam.Data.ContentsOverviewData
 
 import com.computer.inu.readit_appjam.R
+import kotlinx.android.synthetic.main.fragment_home.*
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -20,12 +26,34 @@ private const val ARG_PARAM2 = "param2"
  */
 class HomeFragment : Fragment() {
 
+    lateinit var contentsRecyclerViewAdapter: ContentsRecyclerViewAdapter
+
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        var dataList: ArrayList<ContentsOverviewData> = ArrayList()
+        dataList.add(
+            ContentsOverviewData(
+                "https://upload.wikimedia.org/wikipedia/commons/0/0d/Hong_Jun-pyo_at_the_Japanese_Prime_Minister%27s_Office_%28Cropped%29",
+                "홍준표의 브랜딩",
+                "brunch.com",
+                3,
+                "디자인"
+            )
+        )
+
+        contentsRecyclerViewAdapter = ContentsRecyclerViewAdapter(context!!, dataList)
+        rv_contents_all.adapter = contentsRecyclerViewAdapter
+        rv_contents_all.layoutManager = LinearLayoutManager(context)
     }
 
 
