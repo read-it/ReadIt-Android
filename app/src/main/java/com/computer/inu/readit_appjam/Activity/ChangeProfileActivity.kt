9ae6d0826e8d47d2ec16cb8cbd47d9bd6e50af73
chloe73 <esom735@gmail.com>
2019-07-02 13:14:37 +0900

    마이페이지 스크랩이랑 하이라이팅 프래그먼트 전환 설정페이지 뷰 짜기 설정페이지에서 프로필 변경이랑 휴지통 뷰 만들기

---
+++ app/src/main/java/com/computer/inu/readit_appjam/Activity/ChangeProfileActivity.kt
@@ -0,0 +1,14 @@
+package com.computer.inu.readit_appjam.Activity
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.computer.inu.readit_appjam.R
+import kotlinx.android.synthetic.main.activity_settings_page.*
+
+class ChangeProfileActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_change_profile)
+    }
+}
