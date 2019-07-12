a3981860faf37d7804aad92166105281e15e0601
KimMooHyeon <dlfb77@gmail.com>
2019-07-13 06:43:35 +0900

    홈 프레그먼트 url 인식 # Conflicts: #	app/src/main/AndroidManifest.xml

--- app/src/main/java/com/computer/inu/readit_appjam/Fragment/HomeFragment.kt
+++ app/src/main/java/com/computer/inu/readit_appjam/Fragment/HomeFragment.kt
@@ -223,6 +223,22 @@ class HomeFragment : Fragment() {
             SettingFlag = 0
 
         }
+        var clipboard = activity!!.getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager?
+        if (SharedPreferenceController.getClip(context!!).isNotEmpty()) {
+            if (SharedPreferenceController.getClip(context!!).toString() == clipboard!!.text.toString()) {
+                //값이 같음
+            } else {
+                SharedPreferenceController.clearClip(context!!)
+                SharedPreferenceController.setClip(context!!, clipboard!!.text.toString())
+                rl_home_linkcopy_box.visibility = View.VISIBLE
+                tv_home_copy_url.text = clipboard!!.text.toString()
+                Handler().postDelayed(Runnable {
+                    val animation: Animation = AnimationUtils.loadAnimation(context, R.anim.up_to_down)
+                    rl_home_linkcopy_box.visibility = View.GONE
+                    rl_home_linkcopy_box.startAnimation(animation)
+                }, 4000)//
+            }
+        }
         // getSortCategory(TabdataList[tab_positon].category_idx!!, sort)
         // getMainStorage()
         //getSortCategory(idx, sort)
