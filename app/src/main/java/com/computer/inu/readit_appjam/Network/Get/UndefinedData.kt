8808ae4f39e1c7eb648b5d3b0bff4cb0699d032b
= <=>
2019-07-12 20:04:56 +0900

    Merge branch 'master' of https://github.com/read-it/ReadIt-Android
    # Conflicts:
    #	app/src/main/AndroidManifest.xml
    #	app/src/main/java/com/computer/inu/readit_appjam/Activity/CategorySettingEditActivity.kt
    #	app/src/main/java/com/computer/inu/readit_appjam/Adapter/CategorySettingRvAdapter.kt
    #	app/src/main/java/com/computer/inu/readit_appjam/Network/NetworkService.kt

--- app/src/main/java/com/computer/inu/readit_appjam/Network/Get/UndefinedData.kt
+++ app/src/main/java/com/computer/inu/readit_appjam/Network/Get/UndefinedData.kt
@@ -1,5 +1,5 @@
 package com.computer.inu.readit_appjam.Network.Get
 
 data class UndefinedData(
-    val content_list: ArrayList<UndefinedContent>
+    val contents_list: ArrayList<UndefinedContent>
 )
